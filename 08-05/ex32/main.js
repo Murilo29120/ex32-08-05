056f1a903bfd8ea967fa1ea6d352a28c9562757b
Murilo29120 <murilo.mendes.rodrigues@escola.pr.gov.br>
2025-05-08 12:28:39 -0300

    Add files via upload

---
+++ 08-05/ex32/main.js
@@ -0,0 +1,8 @@
+function adicionartarefa(){
+    const input = document.getElementById('nova-tarefa');
+    const textoTarefa = input.valuue.trim();
+}
+
+if(textoTarefa === "") return;
+
+-- paramos na linha 71----
